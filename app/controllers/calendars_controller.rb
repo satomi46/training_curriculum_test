8ad5de28c8481d26ccab440eeeb5e7bcb51ddcac
satomi46 <satohat@gmail.com>
2022-11-07 18:04:21 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan) #requireメソッドの引数calendarsをplanに修正
   end
 
   def getWeek
